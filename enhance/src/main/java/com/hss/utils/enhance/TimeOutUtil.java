b609b190e40ba9a243ab4441b89496e44be9c793
hss01248 <hss012489@gmail.com>
2022-08-11 11:06:35 +0800

    feat(lifecycle): 超时工具类

---
+++ enhance/src/main/java/com/hss/utils/enhance/TimeOutUtil.java
@@ -0,0 +1,52 @@
+package com.hss.utils.enhance;
+
+import androidx.annotation.Nullable;
+
+import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.ThreadUtils;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Despciption todo
+ * @Author hss
+ * @Date 11/08/2022 10:51
+ * @Version 1.0
+ */
+public class TimeOutUtil {
+
+    /**
+     *
+     * @param hasCallbacked boolean[]{false},构建在栈中,保持线程安全. 正常任务执行后,应该置为true
+     * @param timeoutMills
+     * @param lifecycledObject activity,context,fragment
+     * @param runnableWhenTimeout
+     */
+    public static  void checkTimeout(boolean[] hasCallbacked, long timeoutMills, @Nullable Object lifecycledObject,@Nullable Runnable runnableWhenTimeout){
+        if(hasCallbacked == null){
+            throw new RuntimeException("hasCallbacked should not be null");
+        }
+        if(hasCallbacked.length != 1){
+            throw new RuntimeException("hasCallbacked.length should  be 1");
+        }
+        ThreadUtils.executeByCachedWithDelay(new SimpleLifeCycledTask<Object>(lifecycledObject) {
+            @Override
+            public Object doInBackground() throws Throwable {
+                return null;
+            }
+
+            @Override
+            protected void onSuccessReally(Object result) {
+                if(hasCallbacked[0]){
+                    LogUtils.v("timeout","之前成功过,本处超时不处理");
+                    return;
+                }
+                hasCallbacked[0] = true;
+                if(runnableWhenTimeout != null){
+                    runnableWhenTimeout.run();
+                }
+            }
+
+        }, timeoutMills, TimeUnit.MILLISECONDS);
+    }
+}
