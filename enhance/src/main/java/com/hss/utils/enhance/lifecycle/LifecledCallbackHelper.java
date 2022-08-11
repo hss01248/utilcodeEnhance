8fbe33cfe4b0ef5b653b47183484dbc96eda7cf6
hss01248 <hss012489@gmail.com>
2022-08-11 10:46:27 +0800

    feat(lifecycle): callback的通用lifecycle帮助类

---
+++ enhance/src/main/java/com/hss/utils/enhance/lifecycle/LifecledCallbackHelper.java
@@ -0,0 +1,57 @@
+package com.hss.utils.enhance.lifecycle;
+
+import android.app.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
+
+public class LifecledCallbackHelper implements DefaultLifecycleObserver {
+
+    /**
+     * activity,context,fragment,LifecycleOwner
+     */
+    public LifecledCallbackHelper(Object lifeCycledObj) {
+        if(lifeCycledObj == null){
+            return;
+        }
+        LifecycleOwner lifecycleOwner = LifecycleObjectUtil.getLifecycleOwnerFromObj(lifeCycledObj);
+        if(lifecycleOwner != null){
+            lifecycleOwner.getLifecycle().addObserver(this);
+        }else {
+            this.lifeCycledObj =  LifecycleObjectUtil.getLifecycledObjectFromObj(lifeCycledObj);
+        }
+    }
+
+    /**
+     * activity,context,fragment,LifecycleOwner
+     */
+    protected Object lifeCycledObj;
+
+
+
+    @Override
+    public void onDestroy(@NonNull LifecycleOwner owner) {
+        DefaultLifecycleObserver.super.onDestroy(owner);
+        lifeCycledObj = null;
+    }
+
+    public boolean hasDestoryed(){
+        if(lifeCycledObj == null){
+            return false;
+        }
+        if(lifeCycledObj instanceof Activity){
+            Activity activity = (Activity) lifeCycledObj;
+            if(activity.isFinishing() || activity.isDestroyed()){
+                return true;
+            }
+        }else if(lifeCycledObj instanceof Fragment){
+            Fragment fragment = (Fragment) lifeCycledObj;
+            if(fragment.isDetached() || fragment.isRemoving()){
+                return true;
+            }
+        }
+        return false;
+    }
+}
