76e39f0e8adef31c5265bfbd31ea01a35979b996
hss01248 <hss012489@gmail.com>
2024-06-11 14:02:35 +0800

    通过远程配置让app crash

---
+++ ext/src/main/java/com/hss01248/utils/ext/PkgListFetcher.java
@@ -0,0 +1,128 @@
+package com.hss01248.utils.ext;
+
+import android.app.Activity;
+import android.text.TextUtils;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import com.blankj.utilcode.util.AppUtils;
+import com.blankj.utilcode.util.GsonUtils;
+import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.SPStaticUtils;
+import com.blankj.utilcode.util.UtilsTransActivity;
+import com.google.gson.reflect.TypeToken;
+import com.hss.utils.enhance.api.MyCommonCallback;
+import com.hss01248.activityresult.GoOutOfAppForResultFragment;
+import com.hss01248.activityresult.InAppResultFragment;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * @Despciption todo
+ * @Author hss
+ * @Date 6/11/24 11:28 AM
+ * @Version 1.0
+ */
+public class PkgListFetcher {
+
+    public static void fetch(String spKey,String url, MyCommonCallback<Boolean> pkgHit){
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(5, TimeUnit.SECONDS)
+                .retryOnConnectionFailure(true)
+                .readTimeout(5,TimeUnit.SECONDS)
+                .build();
+        client.newCall(new Request.Builder()
+                .url(url)
+                .get().build()).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                LogUtils.w(e);
+                readCache(spKey, pkgHit);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    readCache(spKey, pkgHit);
+                    return;
+                }
+                String string = response.body().string();
+                parseStr(string,true,spKey,pkgHit);
+            }
+        });
+    }
+
+
+    public static  boolean isForResult(Activity activity) {
+        if (activity instanceof UtilsTransActivity) {
+            return true;
+        }
+        if(activity instanceof FragmentActivity){
+            FragmentActivity activity1 = (FragmentActivity) activity;
+            List<Fragment> fragments = activity1.getSupportFragmentManager().getFragments();
+            LogUtils.i(fragments);
+            for (Fragment fragment : fragments) {
+                if(fragment.isAdded()){
+                    if(fragment instanceof InAppResultFragment){
+                        LogUtils.w("have a InAppResultFragment",fragment);
+                        return true;
+                    }else if(fragment instanceof GoOutOfAppForResultFragment){
+                        LogUtils.w("have a GoOutOfAppForResultFragment",fragment);
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private static void readCache(String spKey, MyCommonCallback<Boolean> pkgHit) {
+        String str =  SPStaticUtils.getString(spKey);
+        if(TextUtils.isEmpty(str) || !str.startsWith("[")){
+            pkgHit.onSuccess(false);
+            return;
+        }
+        try {
+            parseStr(str,false, spKey, pkgHit);
+        } catch (IOException ex) {
+            LogUtils.w(ex);
+        }
+    }
+
+
+    private static void parseStr(String string, boolean fromNet,String spKey,
+                          MyCommonCallback<Boolean> pkgHit) throws IOException{
+        boolean should = false;
+        try {
+            List<String> list = GsonUtils.fromJson(string, new TypeToken<List<String>>() {
+            }.getType());
+            if(fromNet){
+                SPStaticUtils.put(spKey,string);
+            }
+            if(list ==null || list.isEmpty()){
+
+            }else{
+                for (String s : list) {
+                    if (s.equals(AppUtils.getAppPackageName())) {
+                        should = true;
+                        break;
+                    }
+                }
+            }
+            pkgHit.onSuccess(should);
+        } catch (Throwable throwable) {
+            if(fromNet){
+                throw new IOException(throwable);
+            }
+        }
+    }
+}
--- lock-app/src/main/java/com/hss01248/lock_app/LockInit.java
+++ lock-app/src/main/java/com/hss01248/lock_app/LockInit.java
@@ -4,24 +4,23 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.startup.Initializer;
 
 import com.blankj.utilcode.util.LogUtils;
 import com.blankj.utilcode.util.SizeUtils;
 import com.blankj.utilcode.util.ThreadUtils;
-import com.blankj.utilcode.util.UtilsTransActivity;
-import com.hss01248.activityresult.GoOutOfAppForResultFragment;
-import com.hss01248.activityresult.InAppResultFragment;
+import com.hss.utils.enhance.api.MyCommonCallback;
 import com.hss01248.toast.MyToast;
+import com.hss01248.utils.ext.PkgListFetcher;
 import com.hss01248.utils.ext.lifecycle.AppFirstActivityOnCreateListener;
 import com.hss01248.utils.ext.lifecycle.BackgroundAndFirstActivityCreatedCallback;
 
@@ -56,98 +55,111 @@ public class LockInit implements Initializer<String> {
     private void init2(Application application) {
         BackgroundAndFirstActivityCreatedCallback
                 .addAppFirstActivityOnCreateListener(new AppFirstActivityOnCreateListener() {
+
+                    @Override
+                    public void onFirtActivityCreated(Activity activity, @Nullable Bundle savedInstanceState) {
+                        AppFirstActivityOnCreateListener.super.onFirtActivityCreated(activity, savedInstanceState);
+                        //fetchConfig(activity);
+                    }
+
                     @Override
                     public void onForegroundBackgroundChanged(Activity activity, boolean changeToBackground) {
                         AppFirstActivityOnCreateListener.super.onForegroundBackgroundChanged(activity, changeToBackground);
+
                         if (changeToBackground) {
                             return;
                         }
                         if(!lock){
                             return;
                         }
-                        if (activity instanceof UtilsTransActivity) {
+                        boolean isForResult = PkgListFetcher.isForResult(activity);
+                        if(isForResult){
                             return;
                         }
-                        if(activity instanceof FragmentActivity){
-                            FragmentActivity activity1 = (FragmentActivity) activity;
-                            List<Fragment> fragments = activity1.getSupportFragmentManager().getFragments();
-                            LogUtils.i(fragments);
-                            for (Fragment fragment : fragments) {
-                                if(fragment.isAdded()){
-                                    if(fragment instanceof InAppResultFragment){
-                                        LogUtils.w("have a InAppResultFragment",fragment);
-                                        return;
-                                    }else if(fragment instanceof GoOutOfAppForResultFragment){
-                                        LogUtils.w("have a GoOutOfAppForResultFragment",fragment);
-                                        return;
-                                    }
-                                }
-                            }
-                        }
-                        EditText editText = new EditText(activity);
-                        editText.setTransformationMethod(PasswordTransformationMethod.getInstance());
-                        int padding = SizeUtils.dp2px(10);
-                        editText.setPadding(padding, padding, padding, padding);
-                        editText.setHint("输入密码");
-                        //startActivity
-                        AlertDialog.Builder builder = new AlertDialog.Builder(activity)
-                                .setTitle("验证")
-                                .setView(editText)
-                                .setPositiveButton("确认", null);
-                        AlertDialog dialog = builder.create();
-                        dialog.setCanceledOnTouchOutside(false);
-                        dialog.setCancelable(false);
-                        dialog.getWindow().setDimAmount(1.0f);
 
 
-                        // Keyboard
-                        final InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
-
-                    // Auto show keyboard
-                        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-                            @Override
-                            public void onFocusChange(View v, boolean isFocused) {
-
-                                if (isFocused) {
-                                    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
-                                }
-                            }
-                        });
-
-
-                        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
-
-                            @Override
-                            public void onShow(DialogInterface dialogInterface) {
-                                ThreadUtils.getMainHandler().postDelayed(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        editText.requestFocus();
-                                    }
-                                },500);
-
-
-                                Button button = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE);
-                                button.setOnClickListener(new View.OnClickListener() {
-
-                                    @Override
-                                    public void onClick(View view) {
-                                        if (editText.getText().toString().trim().equals(pp+"")) {
-                                            // Hide keyboard
-                                            imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
-                                            dialogInterface.dismiss();
-                                            //下面这个也可以
-                                            //imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
-                                        } else {
-                                            MyToast.error("密码错误");
-                                        }
-                                    }
-                                });
-                            }
-                        });
-                        dialog.show();
+                        showLockDialog(activity);
                     }
                 });
 
+    }
+
+
+
+    private static void showLockDialog(Activity activity) {
+        EditText editText = new EditText(activity);
+        editText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        int padding = SizeUtils.dp2px(10);
+        editText.setPadding(padding, padding, padding, padding);
+        editText.setHint("输入密码");
+        //startActivity
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity)
+                .setTitle("验证")
+                .setView(editText)
+                .setPositiveButton("确认", null);
+        AlertDialog dialog = builder.create();
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.setCancelable(false);
+        dialog.getWindow().setDimAmount(1.0f);
+
+
+        // Keyboard
+        final InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        // Auto show keyboard
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean isFocused) {
+
+                if (isFocused) {
+                    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+                }
+            }
+        });
+
+
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                ThreadUtils.getMainHandler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        editText.requestFocus();
+                    }
+                },500);
+
+
+                Button button = ((AlertDialog) dialog).getButton(AlertDialog.BUTTON_POSITIVE);
+                button.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+                        if (editText.getText().toString().trim().equals(pp+"")) {
+                            // Hide keyboard
+                            imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+                            dialogInterface.dismiss();
+                            //下面这个也可以
+                            //imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+                        } else {
+                            MyToast.error("密码错误");
+                        }
+                    }
+                });
+            }
+        });
+        dialog.show();
+    }
+
+    private void fetchConfig(Activity activity) {
+        PkgListFetcher.fetch("lockxx",
+                "https://oss-kodo.hss01248.tech/2024/locklist.json",
+                new MyCommonCallback<Boolean>() {
+                    @Override
+                    public void onSuccess(Boolean aBoolean) {
+                        LogUtils.w("result:"+aBoolean);
+                        lock = aBoolean;
+                    }
+                });
     }
 }
